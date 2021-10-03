4ce5302a649db561e3f297c7417e7585fe8cb1e2
yann-qu <yannqu@qq.com>
2021-10-03 19:03:56 +0800

    新增补丁centeredmaster并修改MODKEY 1. 新增补丁centeredmaster。注意：改补丁和vanitygaps补丁冲突。centeredmaster补丁提供的函数在vanitygaps补丁中已经定义了，因此打补丁后会出现函数重定义。解决方法：只需要将补丁中的两行按键定义部分加入config.h即可。注意其下标要和layouts数组中centeredmaster布局和centeredfloatingmaster布局分别对应。 2. 将MODKEY修改为Mod1Mask(即ALT键)，解决了一些按键冲突。

--- config.h
+++ config.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -121,6 +121,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[11]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[12]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
